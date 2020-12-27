19f75935bfd62e24b6c56f5d5b98707c67e0a1fd
Romulo Sobreira <romurevoltado@gmail.com>
2020-12-27 09:20:05 -0300

    configurações de ambiente

--- repository/Conexao.php
+++ repository/Conexao.php
@@ -2,6 +2,18 @@
 
 class Conexao{
     public static function criar():PDO{
-        return new PDO("sqlite:db/filmes.db");
+        $env = parse_ini_file('.env');
+        $databaseType = $env["databasetype"];
+        $database = $env["database"];
+        $server = $env["server"];
+        $pass = $env["pass"];
+        $user = $env["user"];
+        
+        #caso o Banco de dados seja o mysql usaremos esse padrão
+        if ($databaseType === "mysql"){
+            $database = "host=$server;dbname=$database";
+        }
+
+        return new PDO("$databaseType:$database", $user, $pass);
     }
 }
